81ba0a5d2682857c8a2a5a9a65f2bb30e29b07b6
Méven Car <meven@kde.org>
2025-10-15 11:23:11 +0200

    KBookmark::updateAccessMetadata: Utilize QDateTime method that is significantly faster
    See https://invent.kde.org/sdk/clazy/-/blob/7d4d980994be106ce690b87f622932c8118382bb/docs/checks/README-qdatetime-utc.md

--- src/kbookmark.cpp
+++ src/kbookmark.cpp
@@ -541,7 +541,7 @@ void KBookmark::updateAccessMetadata()
 {
     // qCDebug(KBOOKMARKS_LOG) << "KBookmark::updateAccessMetadata " << address() << " " << url();
 
-    const uint timet = QDateTime::currentDateTimeUtc().toSecsSinceEpoch();
+    const quint64 timet = QDateTime::currentSecsSinceEpoch();
     setMetaDataItem(QStringLiteral("time_added"), QString::number(timet), DontOverwriteMetaData);
     setMetaDataItem(QStringLiteral("time_visited"), QString::number(timet));
 
